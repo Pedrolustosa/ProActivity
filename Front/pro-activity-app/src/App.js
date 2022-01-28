3cafc0c85173202d3f88d05d9776fa68cca3c973
Pedro Henrique Lustosa E Silva <p.lustosa.e.silva@avanade.com>
2022-01-28 15:45:25 -0300

    First style

--- Front/pro-activity-app/src/App.js
+++ Front/pro-activity-app/src/App.js
@@ -1,21 +1,20 @@
 import './App.css';
 
 function App() {
+  const Activy = [
+    {
+      Id: 1,
+      Description: "Bem-Vindo!",
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React ola
-        </a>
-      </header>
+    <div className="mt-3">
+        <ul className="list-group">
+          <li className="list-group-item">{Activy[0].Id} {Activy[0].Description}</li>
+          <li className="list-group-item" onClick={() => console.log('JSX')}>Segunda Atividade</li>
+          <li className="list-group-item">Terceira Atividade</li>
+          <li className="list-group-item">Quarta Atividade</li>
+        </ul>
     </div>
   );
 }
--- Front/pro-activity-app/src/index.js
+++ Front/pro-activity-app/src/index.js
@@ -1,12 +1,12 @@
+import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <div className='container'>
+      <App />
+    </div>,
   document.getElementById('root')
 );
