450d0759d124690703b72697d075389c28ee699c
Pedro Henrique Lustosa E Silva <p.lustosa.e.silva@avanade.com>
2022-01-28 16:47:58 -0300

    function for demonstration - newActivity()

--- Front/pro-activity-app/src/App.js
+++ Front/pro-activity-app/src/App.js
@@ -11,12 +11,22 @@ function App() {
       Description: "Bem-Vindo! 2",
     },
   ];
+  function newActivity(e){
+    e.preventDefault();
+    
+    const activy = {
+      Id: document.getElementById('id').value,
+      Description: document.getElementById('description').value,
+    }
+    activy.push(activy);
+  }
+
   return (
     <>
       <form>       
         <input id="id" type="text" placeholder='ID'/>
         <input id="description" type="text" placeholder='Descrição'/>
-        <button>+ Atividade</button>
+        <button onClick={newActivity}>+ Atividade</button>
       </form>
       <hr/>
       <div className="mt-3">
