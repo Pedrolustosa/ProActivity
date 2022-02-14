0019272da0f256d1aa3b86519ad21c9131fc2a7c
Pedro Henrique Lustosa E Silva <p.lustosa.e.silva@avanade.com>
2022-02-14 18:45:00 -0300

    First Controller

---
+++ Back/src/ProActivity.API/Controllers/ActivityController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProActivity.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ActivityController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Primeiro Metódo GET";
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "Primeiro Metódo POST";
+        }
+
+        [HttpPut]
+        public string Put()
+        {
+            return "Primeiro Metódo PUT";
+        }
+
+        [HttpDelete]
+        public string Delete()
+        {
+            return "Primeiro Metódo DELETE";
+        }
+
+    }
+}
