f6549c74b22529f95b88cc10896b8c7de5d4322a
Pedrolustosa <pedroeternalss@gmail.com>
2022-03-03 14:14:58 -0300

    Solutions and Projects

---
+++ Back/src/ProActivity.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ProActivity.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ Back/src/ProActivity.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ProActivity.Domain
+{
+    public class Class1
+    {
+    }
+}
