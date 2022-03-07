5e5800b226c60768cf3b2ecb6ff365539382f73f
Pedrolustosa <pedroeternalss@gmail.com>
2022-03-06 21:41:14 -0300

    Creating ActivityRepo

--- Back/src/ProActivity.Data/Repositories/ActivityRepo.cs
+++ Back/src/ProActivity.Data/Repositories/ActivityRepo.cs
@@ -1,25 +1,49 @@
+using System.Linq;
 using System.Threading.Tasks;
+using ProActivity.Data.Context;
 using ProActivity.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 using ProActivity.Domain.Interfaces.Repositories;
 
 namespace ProActivity.Data.Repositories
 {
     public class ActivityRepo : GeneralRepo, IActivityRepo
     {
-
-        public Task<Activity[]> TakeAllAsync()
+        private readonly DataContext _context;
+        public ActivityRepo(DataContext context) : base(context)
         {
-            throw new System.NotImplementedException();
+            _context = context;
         }
 
-        public Task<Activity> TakeByIdAsync(int id)
+        public async Task<Activity[]> TakeAllAsync()
         {
-            throw new System.NotImplementedException();
+            IQueryable<Activity> query = _context.Activities;
+
+            query = query.AsNoTracking()
+                         .OrderByDescending(activ => activ.Id);
+
+            return await query.ToArrayAsync();
         }
 
-        public Task<Activity> TakeByTitleAsync(string title)
+        public async Task<Activity> TakeByIdAsync(int id)
         {
-            throw new System.NotImplementedException();
+            IQueryable<Activity> query = _context.Activities;
+
+            query = query.AsNoTracking()
+                         .OrderByDescending(activ => activ.Id)
+                         .Where(a => a.Id == id);
+
+            return await query.FirstOrDefaultAsync();
+        }
+
+        public async Task<Activity> TakeByTitleAsync(string title)
+        {
+            IQueryable<Activity> query = _context.Activities;
+
+            query = query.AsNoTracking()
+                         .OrderByDescending(activ => activ.Id);
+
+            return await query.FirstOrDefaultAsync(a => a.Title == title);
         }
     }
 }
