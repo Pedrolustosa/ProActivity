a320f4eda628fe99e683f5e112c5ffc519d3b824
Pedrolustosa <pedroeternalss@gmail.com>
2022-03-15 20:19:43 -0300

    Install React-Router-Dom

--- Front/pro-activity-app/src/App.js
+++ Front/pro-activity-app/src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
 import Activity from "./pages/activity/Activity";
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 export default function App() {
   return (
   <>
+    <Router>
+      <Route path='/home' component={Home}></Route>
+      <Route path='/cliente' component={Client}></Route>
+      <Route path='/activity' component={Activity}></Route>
+    </Router>
+
     <Activity/>
   </>
   )
 }
+
+const Home = () => (
+  <div>
+    <h1>Home</h1>
+  </div>
+)
+const Client = () => (
+  <div>
+    <h1>Cliente</h1>
+  </div>
+)
