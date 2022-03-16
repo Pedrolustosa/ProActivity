26799f98a4e468bc7d17a20121d0af4ee3cae973
Pedrolustosa <pedroeternalss@gmail.com>
2022-03-15 21:54:47 -0300

    Active Class Name

--- Front/pro-activity-app/src/components/Menu.jsx
+++ Front/pro-activity-app/src/components/Menu.jsx
@@ -9,8 +9,8 @@ export default function Menu() {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link as={NavLink} to='/client'>Cliente</Nav.Link>
-                        <Nav.Link as={NavLink} to='/activity'>Atividades</Nav.Link>
+                        <Nav.Link activeClassName='active' as={NavLink} to='/client'>Cliente</Nav.Link>
+                        <Nav.Link activeClassName='active' as={NavLink} to='/activity'>Atividades</Nav.Link>
                     </Nav>
                     <Nav>
                         <NavDropdown align='end' title="Pedro Lustosa" id="basic-nav-dropdown">
