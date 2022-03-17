92ff443c8fb3f821db2adfcca885cb0504e70ea6
Pedrolustosa <pedroeternalss@gmail.com>
2022-03-16 21:04:32 -0300

    New routes

--- Front/pro-activity-app/src/App.js
+++ Front/pro-activity-app/src/App.js
@@ -3,13 +3,15 @@ import { Switch, Route } from 'react-router-dom';
 import Client from './pages/client/Client';
 import Activity from "./pages/activity/Activity";
 import Dashboard from './pages/dashboard/Dashboard';
+import ClientForm from './pages/client/ClientForm';
 
 export default function App() {
   return (
     <Switch>
       <Route path='/' exact component={Dashboard}/>
-      <Route path='/client' component={Client}/>
-      <Route path='/activity' component={Activity}/>
+      <Route path='/activity/list' component={Activity}/>
+      <Route path='/client/list' component={Client}/>
+      <Route path='/client/detail' component={ClientForm}/>
     </Switch>
   );
 }
