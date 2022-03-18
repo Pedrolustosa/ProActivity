b19a6cd69204e1a512c602a6398975fc03bdc4fe
Pedrolustosa <pedroeternalss@gmail.com>
2022-03-18 14:25:43 -0300

    Dashboard

--- Front/pro-activity-app/src/components/TitlePage.jsx
+++ Front/pro-activity-app/src/components/TitlePage.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function TitlePage({ title, children }) {
     return (
-        <div className='d-flex justify-content-between align-items-end mt-2 mb-3 pb-2 border-bottom border-1'>
-            <h1 className='m-0 p-0'>{title}</h1>
+        <div className='d-flex justify-content-between align-items-end mt-4 mb-3 pb-2 border-bottom border-1'>
+            <h1 className='m-0 p-1'>{title}</h1>
             {children}
         </div>
     );
--- Front/pro-activity-app/src/pages/dashboard/Dashboard.jsx
+++ Front/pro-activity-app/src/pages/dashboard/Dashboard.jsx
@@ -1,11 +1,84 @@
-import React from 'react'
-import TitlePage from '../../components/TitlePage'
+import React from 'react';
+import TitlePage from '../../components/TitlePage';
+import { Card, Row, Col } from 'react-bootstrap';
 
 export default function Dashboard() {
     return (
         <>
-            <TitlePage title='Dashboard' />
-            <div>Card Here, Waiting...</div>
+            <TitlePage title='Dashboard'/>
+            <div className='mt-5'>
+                <Row>
+
+                    <Col>
+                        <div className="shadow p-1 mb-5 bg-body rounded">
+                            <Card border='success' >
+                                <Card.Header className='bg-success'>
+                                    <h5 className='text-center fw-bold'>
+                                        Clientes atuais
+                                    </h5>
+                                </Card.Header>
+                                <Card.Body>
+                                    <Card.Title>
+                                        <h1 className='text-center'>25</h1>
+                                    </Card.Title>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                    </Col>
+
+                    <Col>
+                        <div className="shadow p-1 mb-5 bg-body rounded">
+                            <Card border='info'>
+                                <Card.Header className='bg-info'>
+                                    <h5 className='text-center fw-bold'>
+                                        Atividades totais
+                                    </h5>
+                                </Card.Header>
+                                <Card.Body>
+                                    <Card.Title>
+                                        <h1 className='text-center'>256</h1>
+                                    </Card.Title>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                    </Col>
+
+                    <Col>
+                        <div className="shadow p-1 mb-5 bg-body rounded">
+                            <Card border='warning'>
+                                <Card.Header className='bg-warning'>
+                                    <h5 className='text-center fw-bold'>
+                                        Atividades Urgentes
+                                    </h5>
+                                </Card.Header>
+                                <Card.Body>
+                                    <Card.Title>
+                                        <h1 className='text-center'>25</h1>
+                                    </Card.Title>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                    </Col>
+
+                    <Col>
+                        <div className="shadow p-1 mb-5 bg-body rounded">
+                            <Card border='danger'>
+                                <Card.Header className='bg-danger'>
+                                    <h5 className='text-center fw-bold'>
+                                        Atividades Atrasadas
+                                    </h5>
+                                </Card.Header>
+                                <Card.Body>
+                                    <Card.Title>
+                                        <h1 className='text-center'>2</h1>
+                                    </Card.Title>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                    </Col>
+
+                </Row>
+            </div>
         </>
-    )
+    );
 }
