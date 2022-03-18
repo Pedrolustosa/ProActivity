155369684f8939231e3991ca2ac8f11e0c6ce626
Pedrolustosa <pedroeternalss@gmail.com>
2022-03-18 14:10:25 -0300

    PageNotFound

--- Front/pro-activity-app/src/App.js
+++ Front/pro-activity-app/src/App.js
@@ -4,6 +4,7 @@ import Client from './pages/client/Client';
 import Activity from "./pages/activity/Activity";
 import Dashboard from './pages/dashboard/Dashboard';
 import ClientForm from './pages/client/ClientForm';
+import PageNotFound from './pages/PageNotFound';
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
       <Route path='/activity/list' component={Activity}/>
       <Route path='/client/list' component={Client}/>
       <Route path='/client/detail/:id?' component={ClientForm}/>
+      <Route component={PageNotFound}/>
     </Switch>
   );
 }
---
+++ Front/pro-activity-app/src/pages/PageNotFound.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function PageNotFound() {
+  return (
+    <h1 className='text-center mt-4 mb-4'>
+      <p>
+        <br />
+        <br />
+        <br />
+        Page Not Found
+        <br />
+        <br />
+        <br />
+        Error 404
+        <br />
+        <br />
+        <br />
+        Página não encontrada!
+      </p>
+    </h1>
+  )
+}
